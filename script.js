0c201199edb5ea2ffc18919af931b6de3f85be43
Nanuka Roinishvili <Nanuka.roinishvili@gmail.com>
2023-02-05 21:14:54 +0400

    first commit

--- script.js
+++ script.js
@@ -16,15 +16,21 @@ const nav = document.querySelector(".nav"),
 for (let i = 0; i < totalNavList; i++) {
   const a = navList[i].querySelector("a");
   a.addEventListener("click", function () {
+    for (let i = 0; i < totalSection; i++) {
+      allSection[i].classList.remove("back-section");
+    }
     for (let j = 0; j < totalNavList; j++)
-      if (navList[j].querySelector("a").classList.contains("active")) {
-        console.log(navList[j].querySelector("a"));
+      if (navList[j].querySelector("a").classList.remove("active")) {
+        allSection[j].classList.add("back-section");
       }
     {
       navList[i].querySelector("a").classList.remove("active");
     }
     this.classList.add("active");
     showSection(this);
+    if (window.innerWidth < 1200) {
+      asideSectionTogglerBtn();
+    }
   });
 }
 
@@ -34,4 +40,32 @@ function showSection(element) {
   }
   const target = element.getAttribute("href").split("#")[1];
   document.querySelector("#" + target).classList.add("active");
+}
+function updateNav(element) {
+  for (let i = 0; i < totalNavList; i++) {
+    navList[i].querySelector("a").classList.remove("active");
+    const target = element.getAttribute("href").split("#")[1];
+    if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
+    {
+      navList[i].querySelector("a").classList.add("active");
+    }
+  }
+}
+document.querySelector(".btn-hire-me").addEventListener("click", function () {
+  const sectionIndex = this.getAttribute("data-section-index");
+  console.log(sectionIndex)
+  showSection(this);
+  updateNav(this);
+});
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+  asideSectionTogglerBtn();
+});
+function asideSectionTogglerBtn() {
+  aside.classList.toggle("open");
+  navTogglerBtn.classList.toggle("open");
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.toggle("open");
+  }
 }
